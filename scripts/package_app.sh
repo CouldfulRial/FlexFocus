a3a3155cda343b2b174106c4e8183cc1dde1fb63
CouldfulRial <yfgcould@myyahoo.com>
2026-02-27 18:07:05 +1100

    添加icon

--- scripts/package_app.sh
+++ scripts/package_app.sh
@@ -10,6 +10,10 @@ CONTENTS_DIR="$APP_DIR/Contents"
 MACOS_DIR="$CONTENTS_DIR/MacOS"
 RESOURCES_DIR="$CONTENTS_DIR/Resources"
 PLIST_PATH="$CONTENTS_DIR/Info.plist"
+ICON_SOURCE="$ROOT_DIR/FlexFocus.png"
+ICON_NAME="FlexFocus"
+ICONSET_DIR="$DIST_DIR/${ICON_NAME}.iconset"
+ICNS_PATH="$RESOURCES_DIR/${ICON_NAME}.icns"
 
 mkdir -p "$DIST_DIR"
 
@@ -21,6 +25,25 @@ mkdir -p "$MACOS_DIR" "$RESOURCES_DIR"
 
 cp "$BUILD_DIR/$APP_NAME" "$MACOS_DIR/$APP_NAME"
 chmod +x "$MACOS_DIR/$APP_NAME"
+
+if [[ -f "$ICON_SOURCE" ]]; then
+    rm -rf "$ICONSET_DIR"
+    mkdir -p "$ICONSET_DIR"
+
+    sips -z 16 16 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_16x16.png" >/dev/null
+    sips -z 32 32 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_16x16@2x.png" >/dev/null
+    sips -z 32 32 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_32x32.png" >/dev/null
+    sips -z 64 64 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_32x32@2x.png" >/dev/null
+    sips -z 128 128 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_128x128.png" >/dev/null
+    sips -z 256 256 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_128x128@2x.png" >/dev/null
+    sips -z 256 256 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_256x256.png" >/dev/null
+    sips -z 512 512 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_256x256@2x.png" >/dev/null
+    sips -z 512 512 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_512x512.png" >/dev/null
+    sips -z 1024 1024 "$ICON_SOURCE" --out "$ICONSET_DIR/icon_512x512@2x.png" >/dev/null
+
+    iconutil -c icns "$ICONSET_DIR" -o "$ICNS_PATH"
+    rm -rf "$ICONSET_DIR"
+fi
 
 cat > "$PLIST_PATH" <<'PLIST'
 <?xml version="1.0" encoding="UTF-8"?>
@@ -35,6 +58,8 @@ cat > "$PLIST_PATH" <<'PLIST'
     <string>com.flexfocus.app</string>
     <key>CFBundleInfoDictionaryVersion</key>
     <string>6.0</string>
+    <key>CFBundleIconFile</key>
+    <string>FlexFocus.icns</string>
     <key>CFBundleName</key>
     <string>FlexFocus</string>
     <key>CFBundlePackageType</key>
